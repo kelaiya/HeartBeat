2f139bff0a73c64949ef507fad50451ac8a3429d
Kelaiya <kelaiyarao1@gmail.com>
2018-02-23 16:30:50 -0500

    commenting codes

--- client/components/Appointments.js
+++ client/components/Appointments.js
@@ -1,44 +1,43 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
 import store, {fetchUser} from '../store'
-// Main component is the initial page displayed by default 
+
+// Appointments component will show details of all the upcoming appointments.
 
 class Appointments extends Component {
 
    render() {
-   	console.log("component", this.props.images)
    	var user = this.props.ans;
-        return (
-            <div className="container">
-              {
-            		user.map(function(data){
-                  var val = ""
-                  if(Number(data.date.slice(11,13)) > 12) val = "pm"
-                  else val = "am"
-            			return (
-            			        <div key={data.id}>
-                   
-                            <h1>Name of the doctor: {data.docName}</h1>
-                            <h1>Date of the appointment: {data.date.slice(0,10)}</h1>
-                            <h1>Time of the appointment: {data.date.slice(11,16)} {val}</h1>
-            							</div>
-          							)
-            		})
-            	}
-            </div>
-        )
-    }
+      return (
+        <div className="container">
+          {
+            user.map(function(data){
+              var val = ""
+              if(Number(data.date.slice(11,13)) > 12) val = "pm"
+              else val = "am"
+            	return (
+            			<div key={data.id}>
+                    <h1>Name of the doctor: {data.docName}</h1>
+                    <h1>Date of the appointment: {data.date.slice(0,10)}</h1>
+                    <h1>Time of the appointment: {data.date.slice(11,16)} {val}</h1>
+            			</div>
+          		)
+            })
+        }
+      </div>
+      )
+  }
 }
 
 const mapStateToProps = function(state, ownProps){
 
+  // state.appointment has all the details of patient.
 	let datas = state.appointment;
   const info = ownProps.match.params.id
   if(info) var ans = datas.filter(data => data.user.id == info)
   return {
 		ans
 	}
-
 }
 
 export default connect(mapStateToProps)(Appointments);
--- client/components/Home.js
+++ client/components/Home.js
@@ -4,69 +4,71 @@ import store, {fetchUser} from '../store'
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar'
 
-// Main component is the initial page displayed by default 
+// Home component is the initial page displayed by the app 
+// Names of all the patients which be displayed on this page
+//User can also sort the names by different factors
 
 class Home extends Component {
   constructor(props){
     super(props)
+    // srtKey will sort the names according to the type selected. 
     this.state = {
       sortKey: ""
     }
     this.handleSortChange = this.handleSortChange.bind(this);
-}
-handleSortChange(event){
+  }
+  
+  handleSortChange(event){
     this.setState({ sortKey: event.target.value})
   }
 
-   componentDidMount() {
+  componentDidMount() {
     const userThunk = fetchUser();
     store.dispatch(userThunk);
-}
-   render() {
-   	// console.log("component", this.props.images)
+  }
+  
+  render() {
    	var user = this.props.user;
     if(this.state.sortKey == "titleA-Z"){
       user.sort((a, b) => b.name < a.name);
     } else if(this.state.sortKey == "titleZ-A"){
       user.sort((a, b) => a.name < b.name);
     } 
-        return (
-            <div className="container">
-              <div className="sort">
-                {
-                  user ? <div>
-                   <div className="sort">
-                    <form onSubmit={this.handleSortSubmit} className="sortclass">
-                      <select value={this.state.value} onChange={this.handleSortChange} className="sortclass">
-                        <option> SORT </option>
-                        <option value="titleA-Z"> By Title (A-Z) </option>
-                        <option value="titleZ-A"> By Title (Z-A) </option>
-                      </select>
-                    </form>
-                   </div>
-                  </div> : <h1 />
-                }
-              </div>  
-
-                {
-              		user.map(function(data){
-              			
-                    return (
-              			        <div key={data.id} className="Search-box">
-              								<img src= {data.image} className="image" />
-              								<Link to={`/${data.id}`}className="text-color">{data.name}</Link>
-              							</div>
-            				)
-              		})
-              	}
-            </div>
+    return (
+        <div className="container">
+          <div className="sort">
+            {
+              user ? <div>
+                <div className="sort">
+                  <form onSubmit={this.handleSortSubmit} className="sortclass">
+                    <select value={this.state.value} onChange={this.handleSortChange} className="sortclass">
+                      <option> SORT </option>
+                      <option value="titleA-Z"> By Title (A-Z) </option>
+                      <option value="titleZ-A"> By Title (Z-A) </option>
+                    </select>
+                  </form>
+                </div>
+              </div> : <h1 />
+              }
+          </div>  
+            {
+              user.map(function(data){
+              	return (
+              		<div key={data.id} className="Search-box">
+              			<img src= {data.image} className="image" />
+              			<Link to={`/${data.id}`}className="text-color">{data.name}</Link>
+              		</div>
+            		)
+              })
+            }
+          </div>
         )
     }
 }
 
 const mapStateToProps = function(state){
-	// images is the value of state in store
-	console.log("map", state)
+	
+  // state.user will give the names of all the patients.
 	return {
 		user : state.user
 	}
